b83fdf9a21ef483fad8b6ccc87e57389cf336427
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-01-11 22:34:36 +0600

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,44 +1,44 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
-import project11_img from '../assets/project_1.png'
-import project12_img from '../assets/project2.png'
-import project13_img from '../assets/project_3.png'
-import project14_img from '../assets/project_4.png'
+// import project1_img from '../assets/project_1.svg'
+// import project2_img from '../assets/project_2.svg'
+// import project3_img from '../assets/project_3.svg'
+// import project4_img from '../assets/project_4.svg'
+// import project5_img from '../assets/project_5.svg'
+// import project6_img from '../assets/project_6.svg'
+import project_1 from '../assets/project_1.png'
+import project2 from '../assets/project2.png'
+import project_3 from '../assets/project_3.png'
+import project_4 from '../assets/project_4.png'
 const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project_1
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project_3
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project_4
     },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
-    },
+    // {
+    //     w_no:5,
+    //     w_name:"Web design",
+    //     w_img:project_5
+    // },
+    // {
+    //     w_no:6,
+    //     w_name:"Web design",
+    //     w_img:project_6
+    // },
 ]
  
 export default mywork_data;
